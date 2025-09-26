333efe2594c34193755811379cabf3c7f3931add
Baba <dsmwaura@gmail.com>
2025-09-26 18:50:43 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Frappe Devsecops Dashboard
+
+Frappe app to leverage ERPNext project to render DOD devsecops dashboard
+
+#### License
+
+MIT
---
+++ frappe_devsecops_dashboard/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Frappe Devsecops Dashboard",
+			"type": "module",
+			"label": _("Frappe Devsecops Dashboard")
+		}
+	]
